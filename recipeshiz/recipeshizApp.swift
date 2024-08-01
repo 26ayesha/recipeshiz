2e348001f2676f5304f8ec2c2a91961ae0bad07a
Ayesha <ayeshamalak26@gmail.com>
2024-08-02 00:48:46 +0530

    init

---
+++ recipeshiz/recipeshizApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeshizApp.swift
+//  recipeshiz
+//
+//  Created by Ayesha Siddiqui on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct recipeshizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
